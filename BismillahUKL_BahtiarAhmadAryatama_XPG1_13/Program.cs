e1a199dd949fd80b64fbd734ef421a13bdba0d50
Ary-27 <aryatamaary@gmail.com>
2026-06-02 13:16:36 +0700

    Class_Outdoor

--- BismillahUKL_BahtiarAhmadAryatama_XPG1_13/Program.cs
+++ BismillahUKL_BahtiarAhmadAryatama_XPG1_13/Program.cs
@@ -1,76 +1,76 @@
 ﻿List<Stand> data_stand = new List<Stand>()
 {
-    new Outdoor("Outdoor-1", 400000, ""),
-    new Outdoor("Outdoor-2", 500000, ""),
-    new Indoor("Indoor-1", 700000, ""),
-    new Indoor("Indoor-2", 800000, ""),
-    new Premium("Premium-1", 1800000, ""),
-    new Premium("Premium-2", 2000000,"")
+    new StandOutdoor("Outdoor-1", 400000),
+    new StandOutdoor("Outdoor-2", 500000),
+    new StandIndoor("Indoor-1", 700000),
+    new StandIndoor("Indoor-2", 800000),
+    new StandPremium("Premium-1", 1800000),
+    new StandPremium("Premium-2", 2000000)
 };
 
 while (true)
 {
     Console.Clear();
 
-    Console.WriteLine("\n======= MOKLET EXPO MANAGEMENT CENTER =======");
-    Console.WriteLine("\nDaftar Stand Tersedia");
+    Console.WriteLine("\n======= MOKLET EXPO MANAGEMENT =======");
+    Console.WriteLine("\nDaftar Stand Bazaar Tersedia");
 
     foreach (var dk in data_stand)
     {
-        dk.tampilkanInfo();
+        dk.TampilkanInfo();
     }
 
     Console.WriteLine("\nPilih Menu: ");
-    Console.WriteLine("1. Sewa\n2. Kembali\n3. Keluar");
+    Console.WriteLine("1. Sewa Stand\n2. Akhiri Sewa\n3. Keluar");
     Console.Write("Pilihan Anda: ");
     string pilihan = Console.ReadLine();
 
-    if (pilihan == "1")
+    if (pilihan == "1") // Proses penyewaan stand
     {
-        Console.Write("\nInput Jenis Stand: ");
+        Console.Write("\nInput nama stand: ");
         string namaStand = Console.ReadLine();
 
-        var cari_stand = data_stand.FirstOrDefault(ck => string.Equals(namaStand, ck.NamaStand, StringComparison.OrdinalIgnoreCase));
-
-        if (cari_stand == null)
-        {
-            Console.WriteLine("\nStand tidak ditemukan!");
-        }
-        else if (cari_stand.IsAvailable)
-        {
-            Console.WriteLine("\nInput jumlah hari sewa: ");
-            int hari = int.Parse(Console.ReadLine());
-
-            double total_sewa = cari_stand.hitungtotal(hari);
-
-            cari_stand.ubahstatus();
-
-            Console.WriteLine($"Total pembayaran sewa: Rp {total_sewa}");
-        }
-        else
-        {
-            Console.WriteLine("\nStand tidak tersedia!");
-        }
-    }
-    else if (pilihan == "2")
-    {
-        Console.Write("\nInput Jenis Stand: ");
-        string namaKendaraan = Console.ReadLine();
-
-        var cari_stand = data_stand.FirstOrDefault(ck => string.Equals(namaKendaraan, ck.NamaStand, StringComparison.OrdinalIgnoreCase));
+        var cari_stand = data_stand.FirstOrDefault(cs => string.Equals(namaStand, cs.NamaStand, StringComparison.OrdinalIgnoreCase));
 
         if (cari_stand == null)
         {
             Console.WriteLine("\nStand tidak ditemukan");
         }
-        else if (!cari_stand.IsAvailable)
+        else if (cari_stand.IsAvailable)
         {
-            cari_stand.ubahstatus();
-            Console.WriteLine("\nStand berhasil dikembalikan");
+            Console.Write("\nInput jumlah hari sewa: ");
+            int hari = int.Parse(Console.ReadLine());
+
+            double total_sewa = cari_stand.HitungTotal(hari);
+
+            cari_stand.UbahStatus();
+
+            Console.WriteLine($"Total pembayaran sewa: Rp {total_sewa}");
         }
         else
         {
-            Console.WriteLine("\nStand pengembalian tidak bisa dilakukan");
+            Console.WriteLine("\nStand sedang tidak tersedia");
+        }
+    }
+    else if (pilihan == "2") // Proses pengembalian stand
+    {
+        Console.Write("\nInput nama stand: ");
+        string namaStand = Console.ReadLine();
+
+        var cari_stand = data_stand.FirstOrDefault(cs => string.Equals(namaStand, cs.NamaStand, StringComparison.OrdinalIgnoreCase));
+
+        if (cari_stand == null)
+        {
+            Console.WriteLine("\nStand tidak ditemukan");
+        }
+        else if (cari_stand.IsAvailable)
+        {
+            cari_stand.UbahStatus();
+            Console.WriteLine("\nSewa stand berhasil diakhiri");
+        }
+        else
+        {
+            Console.WriteLine("\nProses pengembalian tidak bisa dilakukan");
         }
     }
     else if (pilihan == "3")
@@ -83,6 +83,78 @@ while (true)
     {
         Console.WriteLine("\nPilihan Invalid!");
     }
+
     Console.WriteLine("\nTekan E N T E R untuk mengulangi proses...");
     Console.ReadLine();
+}
+
+// CLASS STAND
+class Stand
+{
+    protected string _namaStand;
+    protected double _hargaSewaPerHari;
+    protected bool _isAvailable;
+
+    public Stand(string namaStand, double hargaSewaPerHari)
+    {
+        _namaStand = namaStand;
+        _hargaSewaPerHari = hargaSewaPerHari;
+        _isAvailable = true;
+    }
+
+    public string NamaStand
+    {
+        get { return _namaStand; }
+        set { _namaStand = value; }
+    }
+
+    public double HargaSewaPerHari
+    {
+        get { return _hargaSewaPerHari; }
+        set
+        {
+            if (value > 0)
+                _hargaSewaPerHari = value;
+            else
+                Console.WriteLine("Harga sewa per hari harus lebih besar dari 0.");
+        }
+    }
+
+    public bool IsAvailable
+    {
+        get { return _isAvailable; }
+    }
+
+    public void TampilkanInfo()
+    {
+        Console.WriteLine($"{NamaStand} | Rp {HargaSewaPerHari} / hari | {(IsAvailable ? "Tersedia" : "Tidak tersedia")}");
+    }
+
+    public void UbahStatus()
+    {
+        _isAvailable = !_isAvailable;
+    }
+
+    public virtual double HitungTotal(int jumlahHari)
+    {
+        return _hargaSewaPerHari * jumlahHari;
+    }
+}
+
+// CLASS OUTDOOR
+class StandOutdoor : Stand
+{
+    private double _biayaTenda;
+    public StandOutdoor(string namaStand, double hargaSewaPerHari) : base(namaStand, hargaSewaPerHari)
+    {
+        _biayaTenda = 75000;
+    }
+    public double BiayaTenda
+    {
+        get { return _biayaTenda; }
+    }
+    public override double HitungTotal(int jumlahHari)
+    {
+        return base.HitungTotal(jumlahHari) + (_biayaTenda * jumlahHari);
+    }
 }
